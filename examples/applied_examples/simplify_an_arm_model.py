0720c81f46e24b2f7d7a1ceff5231f006f3a1d22
eve-mac <eve.charbonneau.1@umontreal.ca>
2026-06-25 15:05:13 -0400

    blacked

--- examples/applied_examples/simplify_an_arm_model.py
+++ examples/applied_examples/simplify_an_arm_model.py
@@ -288,6 +288,7 @@ def create_planar_model(
     # The only constraints are that the origin, insertion and via points are defined on the same segments.
     # In that case, all properties of the muscle are the mean of fascicles except for maximal force which is the sum of fascicles
 
+
 def main():
 
     # Configure logging
